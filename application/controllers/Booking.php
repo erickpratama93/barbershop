a77a40f89f737a3acc144f15e87b56293a77362b
erickpratama93 <72060419+erickpratama93@users.noreply.github.com>
2023-02-11 15:06:13 +0700

    first commit

---
+++ application/controllers/Booking.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Booking extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Booking Antrian';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('booking/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function riwayat()
+    {
+        $data['title'] = 'Riwayat';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('booking/riwayat', $data);
+        $this->load->view('templates/footer');
+    }
+}
---
+++ application/controllers/User.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Home';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function profil()
+    {
+        $data['title'] = 'My Profile';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/profil', $data);
+        $this->load->view('templates/footer');
+    }
+    public function editProfile()
+    {
+        $data['title'] = 'Edit Profile';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/editProfile', $data);
+        $this->load->view('templates/footer');
+    }
+}
